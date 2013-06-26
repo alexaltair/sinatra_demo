d1b7d65bda25c20d2e0ef6ddd7bbf15bd9ca621b
Alex Altair <alexanderaltair@gmail.com>
2013-06-26 14:47:49 -0700

    First commit of our first Sinatra project. App demos working with the OMDB API.

---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'open-uri'
+
+get '/' do 
+  @name = "Alex Altair"
+  erb :home
+end
+
+def get_model(q)
+  url = "http://www.omdbapi.com/?s=#{URI.escape(q)}"
+  results = JSON.load(open(url).read)
+  results["Search"]
+end
+
+get '/search' do 
+  @query = params[:q]
+  @movies = get_model(@query)
+  erb :result
+end
+
+def get_movie(link)
+  url = "http://www.omdbapi.com/?i=#{URI.escape(link)}"
+  JSON.load(open(url).read)
+end
+
+get '/movie/:imdbID' do
+  @link = params[:imdbID]
+  @movie = get_movie(@link)
+  erb :movie
+end
